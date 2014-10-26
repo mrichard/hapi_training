14a85558243ceef96517778568bacd052fcb84ec
Michel Richard <mrichard4@LM3000026417.local>
2014-10-26 18:47:24 -0400

    simple hapi server

---
+++ index.js
@@ -0,0 +1,23 @@
+var Hapi = require('hapi');
+var server = new Hapi.Server(3000);
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
+
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
